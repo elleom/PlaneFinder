e548cf3b0e347158cd65f0b7800a0374b9a17ead
el_le <el_leo_m@hotmail.com>
2023-04-06 18:47:35 +0300

    initial commit

---
+++ src/main/java/eu/oelleo/pf/PfApplication.java
@@ -0,0 +1,13 @@
+package eu.oelleo.pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfApplication.class, args);
+	}
+
+}
